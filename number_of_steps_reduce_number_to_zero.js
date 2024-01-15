a96d0d5d9abf35f95b8901575f08164507a96a6d
shariarhossain17 <shariarhossain23@gmail.com>
2024-01-15 16:53:54 +0600

    number of steps

---
+++ number_of_steps_reduce_number_to_zero.js
@@ -0,0 +1,18 @@
+const numberOfStepsNumbersToZero = (number) =>{
+
+    let cnt=0;
+    while(number!=0){
+        if(number%2==0){
+            number /= 2;
+            cnt++;
+        }
+        else{
+            number-=1;
+            cnt++;
+        }
+    }
+
+    return cnt;
+}
+
+console.log(numberOfStepsNumbersToZero(14));
